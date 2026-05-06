d24c3521ee65b7cabe6b2bfdc5fac94202ab9e17
GiacomoVisin <killerboss1150@gmail.com>
2026-05-06 13:11:29 +0200

    adding the list mapped and the button for adding the task

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
+import { useState } from "react"
+
+const initialTask = [`Buy a CPU`, `Sell the mouse`, `Fix the stereo`]
 
 
 function App() {
+
+  const [newTask, setNewTask] = useState(``)
+  const [task, setTask] = useState(initialTask)
+
+  function handleSubmit(e){
+    e.preventDefault()
+    setTask([newTask, ...task])
+    setNewTask("")
+  }
+
+
 
 
   return (
     <>
       <div className="container">
-        <ul class="list-group">
-          <li class="list-group-item">Active item</li>
-          <li class="list-group-item">Item</li>
-          <li class="list-group-item ">Disabled item</li>
-        </ul>
-        
+        <h1> ToDo List</h1>
+        <div className="card">
+          <form action="" onSubmit={handleSubmit}>
+            <div className="d-flex gap-1 align-items-stretch mb-2">
+              <input type="text" className="form-control" value={newTask} onChange={e => setNewTask(e.target.value)} placeholder="add new task"/>
+              <button className="btn btn-dark"> Add new Task</button>
+            </div>
+          </form>
+          <ul className="list-group">
+            {task.map((task, i) => (
+              <li className="list-group-item d-flex justify-content-between" key={i}>
+                {task} <button className="btn btn-danger"><i className="bi bi-trash-fill"></i> </button>
+                </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </>
   )
