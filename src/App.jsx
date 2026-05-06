82e3c6965d46838f8f0046737f7239a4253be882
GiacomoVisin <killerboss1150@gmail.com>
2026-05-06 13:10:10 +0200

    fixing icons

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,14 @@ function App() {
 
   return (
     <>
-    
+      <div className="container">
+        <ul class="list-group">
+          <li class="list-group-item">Active item</li>
+          <li class="list-group-item">Item</li>
+          <li class="list-group-item ">Disabled item</li>
+        </ul>
+        
+      </div>
     </>
   )
 }
